9137beb2933f44b534afb31b64a38167ff4de73c
Zach <100164907@ccisd.net>
2025-08-15 13:32:04 -0500

    Add files via upload

---
+++ Pyware.py
@@ -0,0 +1,29 @@
+import time
+import os
+
+print("--PyWare--")
+Run = input("This is SPYWARE are you sure you want to run? Y/N | ")
+if Run.upper() == "Y":
+    Confirm = input("Are you POSITIVE you want to run this? Y/N | ")
+    if Confirm.upper() == "Y":
+        print("Test")
+    elif Confirm.upper() == "N":
+        os.system('cls')
+        print("Closing program please wait...")
+        time.sleep(3)
+        print("")
+    else:
+        os.system('cls')
+        print("Closing program please wait,,,")
+        time.sleep(3)
+        print("")
+elif Run.upper() == "N":
+    os.system('cls')
+    print("Closing program please wait...")
+    time.sleep(3)
+    print("")
+else:
+    os.system('cls')
+    print("Closing program please wait...")
+    time.sleep(3)
+    print("")
